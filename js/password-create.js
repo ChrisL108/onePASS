14158e115bdc659a66edb7a93b69f0691f37fecf
Christopher Lacaille <Christopher Lacaille>
2016-04-27 23:13:16 -0500

    update to generateRandom() & add getRandChar() in Password object

--- js/password-create.js
+++ js/password-create.js
@@ -1,29 +1,39 @@
 $(function() {
 
+	var $genBtn = $("#generatePw");
+
+	var $userPw = $("#userPw");
 
 	Password = {
-		extraSymbols : ["!", "?", "#", "$", "*"],
 		
-		generateRandom: function() { // generates a random password
-			var randomSymbol = this.extraSymbols[Math.floor(Math.random() * this.extraSymbols.length)];
-			var randomSymbol2 = this.extraSymbols[Math.floor(Math.random() * this.extraSymbols.length)];
-			var randomPassword = Math.random().toString(36).slice(-8) + randomSymbol + 
-				   				 Math.random().toString(36).slice(-4).toUpperCase() +
-				   				 randomSymbol2 ;
-			return randomPassword;
+		// generates a random password
+		generateRandom: function() { 
+			symbols = "![]{}()%&*$#^<>~@|";
+			letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			numbers = "0123456789";
+			var password = [];
+
+			for (var i = 0, len = 16 ; i < len ; i++) {
+				if (password.length < (len - 6)){
+					password += this.getRandChar(letters);
+				}else if (password.length < (len - 2)) {
+					password += this.getRandChar(numbers);
+				} else {
+					password += this.getRandChar(symbols);
+				}
+			}
+			return password;
 		},
 
-		create: function(service, password) {
-			var date = new Date();
-			// if no password is provided, generate one
-			if (!password) password = this.generateRandom();
-
-			return "Password for "+service+
-					" is '"+password+"'\n"+
-					"Generated on "+date;
-		}
+		// gets random character from string
+		getRandChar : function(elems) {
+			return elems[Math.floor(Math.random() * elems.length)];
+		},
 	};
 
-	console.log(Password.create("Twitter"));
+	// Generate Button
+	$genBtn.on('click', function() {
+		$userPw.val(Password.generateRandom());
+	});
 
 });
