52a7c30f49f8bc4cfdafc125ccef6c8b3153396f
Christopher Lacaille <Christopher Lacaille>
2016-07-17 01:44:55 -0500

    small change

--- js/password-create.js
+++ js/password-create.js
@@ -45,7 +45,7 @@ $(function() {
 			for (var i = 0, len = 16 ; i < len ; i++) {
 				if (password.length < (len - 6)){
 					password += this._getRandChar(letters);
-				}else if (password.length < (len - 3)) {
+				} else if (password.length < (len - 3)) {
 					password += this._getRandChar(numbers);
 				} else {
 					password += this._getRandChar(symbols);
