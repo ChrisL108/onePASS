fde8d6fb51126e21ee6c1daa77f6589c3c130bc9
Christopher Lacaille <Christopher Lacaille>
2016-07-24 12:49:35 -0500

    run eslint on password-create.js

--- js/password-create.js
+++ js/password-create.js
@@ -1,23 +1,23 @@
 $(function() {
 
-	var $pwList = $('#password_list'),  // List of user's passwords
+	var $pwList = $("#password_list"),  // List of user's passwords
 		$userPw = $("#userPw"), // Input box for passwords
-		$service = $('#pwLabel'), // Input box for "for" service
-		$popIn = $('#pop-in'), // Fade in - shows added password
-		$editPassBox = $('#editPass'), // Fade in - shows 'edit pw' box
+		$service = $("#pwLabel"), // Input box for "for" service
+		$popIn = $("#pop-in"), // Fade in - shows added password
+		$editPassBox = $("#editPass"), // Fade in - shows 'edit pw' box
 		$elemToEdit, $localStorageItem, // Need variables for dynamically added passwords
 		$dummyPw = $(".dummy-pw");
 
-		$dummyPw.hide();
-		$popIn.hide();
-		$editPassBox.hide();
+	$dummyPw.hide();
+	$popIn.hide();
+	$editPassBox.hide();
 
 		//  Character sets for password generation
-		symbols = "![]{}()%&*$#^<>~@|",
+	var symbols = "![]{}()%&*$#^<>~@|",
 		letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
 		numbers = "0123456789";
 
-		var service, pw;
+	var service, pw;
 
 	var Password = {
 		// if ( passwords in localstorage ):
@@ -55,67 +55,66 @@ $(function() {
 		},
 		// add password to list
 		addPassword: function(pw, service) {
-			var listItem =  '<li class="list_item"><span class="list-pw">' + pw + 
-							'</span>  <small>' + service + '</small>' + 
-							'<span class="glyphicon glyphicon-remove text-danger"></span>' + 
-							' <span class="glyphicon glyphicon-pencil text-primary"></span></li>';
+			var listItem =  "<li class=\"list_item\"><span class=\"list-pw\">" + pw + 
+							"</span>  <small>" + service + "</small>" + 
+							"<span class=\"glyphicon glyphicon-remove text-danger\"></span>" + 
+							" <span class=\"glyphicon glyphicon-pencil text-primary\"></span></li>";
 			$pwList.append(listItem);
 			// set in localstorage
 			localStorage.setItem(service, pw);
 		}
 	}; // end Password()
 
-Password.init();
+	Password.init();
 
 // ~~~~~ EVENT HANDLERS ~~~~~
 
 	// Generate Password
-	$("#generatePw").on('click', function() {
+	$("#generatePw").on("click", function() {
 		$userPw.val(Password.generateRandom());
 	});
 
 	// (SUBMIT) new-password Button
-	$('button#newPassSubmit').on('click', function(e) {
+	$("button#newPassSubmit").on("click", function(e) {
 		e.preventDefault();
-		Password.addPassword($userPw.val(), $service.val());
-		console.log("test");
+		Password.addPassword( $userPw.val(), $service.val() );
 		// Pop-In notification
 		if ($service.val()) {
-			$popIn.find('h1').html("Password Added for "+$service.val()+ "!");
+			$popIn.find("h1").html("Password Added for "+$service.val()+ "!");
 		} else {
-			$popIn.find('h1').html("Password Added!");
+			$popIn.find("h1").html("Password Added!");
 		}
 		// fade in notification
-		$popIn.stop(true).fadeIn('slow').delay(1000).fadeOut('slow');
+		$popIn.stop(true).fadeIn("slow").delay(1000).fadeOut("slow");
 		// clear inputs & focus top input
 		$userPw.val("").focus();
 		$service.val("");
 	});
 
 	// (DELETE) buttons
-	$('body').on('click', '.glyphicon-remove', function() {
-		var $item = $(this).closest('li');
-		$item.fadeOut('fast', function() {$item.remove();});
+	$("body").on("click", ".glyphicon-remove", function() {
+		var $item = $(this).closest("li");
+		$item.fadeOut("fast", function() {$item.remove();});
 
-		var itemKey = $item.find('small').text();
+		var itemKey = $item.find("small").text();
 		// remove from localstorage
 		localStorage.removeItem(itemKey);
 	});
 
 	// (EDIT) buttons
-	$('body').on('click', '.glyphicon-pencil', function() {
+	$("body").on("click", ".glyphicon-pencil", function() {
 		$editPassBox.fadeToggle();
-		$elemToEdit = $(this).closest('li');
+		$elemToEdit = $(this).closest("li");
 		// get item to use w/ localstorage
-		$localStorageItem = $elemToEdit.find('small').text();
+		$localStorageItem = $elemToEdit.find("small").text();
 	});
 
 	// (EDIT - SUBMIT) pop-in box
-	$('#editPassSubmit').on('click', function(e) {
+	$("#editPassSubmit").on("click", function(e) {
 		e.preventDefault();
-		var $inputTxt = $('#editPassText');
-		$elemToEdit.children('.list-pw').html($inputTxt.val());
-		$editPassBox.delay(100).fadeOut('slow');
+		var $inputTxt = $("#editPassText");
+		$elemToEdit.children(".list-pw").html($inputTxt.val());
+		$editPassBox.delay(100).fadeOut("slow");
 		// set new password in localstorage
 		localStorage.setItem( $localStorageItem, $inputTxt.val() );
 		// clear input box
@@ -123,7 +122,7 @@ Password.init();
 	});
 
 	// Clear Passwords / Storage
-	$("#clearStorage").on('click', function(e) {
+	$("#clearStorage").on("click", function(e) {
 		e.preventDefault();
 		$(".list_item").remove();
 		localStorage.clear();
